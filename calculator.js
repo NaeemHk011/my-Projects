32679a5c5367d36214d8a298c74e0fb93799fc19
NaeemHk011 <itxkhaskheli@gmail.com>
2023-11-29 14:18:55 +0500

    Calculator

--- calculator.js
+++ calculator.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var prompt = require('prompt-sync')();
-let n1 = parseInt(prompt("Add your First Value: "));
-let n2 = parseInt(prompt("Add your second Value: "));
-let operator = prompt("Please select your operator: ");
+var n1 = parseInt(prompt("Add your First Number: "));
+var n2 = parseInt(prompt("Add your second Number: "));
+var operator = prompt("Please select your operator: ");
 if (operator == "+") {
     console.log(n1 + n2);
 }
